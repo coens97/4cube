3a78bd67305ca4283d7e272f26063bda2f4715b8
agnesaba <agneswadee@gmail.com>
2016-04-27 08:59:25 +0200

    +Start of simulation in business layer

---
+++ Code/4cube.Bussiness/Simulation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _4cube.Bussiness
+{
+    class Simulation : ISimulation
+    {
+        private GridEntity _grid;
+
+        public void ChangeSpeed(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
