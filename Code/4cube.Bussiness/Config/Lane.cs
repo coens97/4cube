2e22fc9ff75e29c9d643b54c16040d659f92bab1
WaterLeung25 <waterleung25@gmail.com>
2016-05-11 15:42:17 +0200

    + EnterPoint, ExitPoint

--- Code/4cube.Bussiness/Config/Lane.cs
+++ Code/4cube.Bussiness/Config/Lane.cs
@@ -8,5 +8,7 @@ namespace _4cube.Bussiness.Config
         public Tuple<int, int, int, int> BoundingBox { get; set; }
         public Direction DirectionLane { get; set; }
         public Direction[] OutgoingDiretion { get; set; }
+        public Tuple<int, int> EnterPoint { get; set; }
+        public Tuple<int, int> ExitPoint { get; set; }
     }
 }
