0c32d286fe1565b198ddb317c446677ce374fe08
inasael <inasael@gmail.com>
2016-05-11 14:00:59 +0200

    * Fixed merrge conflict

--- Code/4cube.Bussiness/Simulation/SimulationUtility.cs
+++ Code/4cube.Bussiness/Simulation/SimulationUtility.cs
@@ -7,14 +7,14 @@ namespace _4cube.Bussiness.Simulation
 {
     public static class SimulationUtility
     {
-        public static bool IsInPosition(this CarEntity car, Tuple<int, int, int, int>[] positions, int x, int y)
+        public static bool IsInPosition(this CarEntity car, Tuple<int, int, int, int>[] positions, int gridx, int gridy)
         {
-            return IsInPosition(car.X, car.Y, positions, x, y);
+            return IsInPosition(car.X, car.Y, positions, gridx, gridy);
         }
 
-        public static bool IsInPosition(this PedestrianEntity ped, Tuple<int, int, int, int>[] positions, int x, int y)
+        public static bool IsInPosition(this PedestrianEntity ped, Tuple<int, int, int, int>[] positions, int gridx, int gridy)
         {
-            return IsInPosition(ped.X, ped.Y, positions, x, y);
+            return IsInPosition(ped.X, ped.Y, positions, gridx, gridy);
         }
 
         public static bool IsInPosition(int x, int y, Tuple<int, int, int, int>[] positions, int gridx, int gridy)
@@ -22,12 +22,23 @@ namespace _4cube.Bussiness.Simulation
             return positions.Any(pos => x.InBetween(pos.Item1 + gridx, pos.Item3 + gridx) && y.InBetween(pos.Item2 + gridy, pos.Item4 + gridy));
         }
 
+        public static Tuple<int, int> GetGridPosition(int x, int y, int gridWidth, int gridHeight)
+        {
+            return new Tuple<int, int>(
+                (int)Math.Floor(x / Convert.ToDecimal(gridWidth)) * gridWidth,
+                (int)Math.Floor(y / Convert.ToDecimal(gridHeight)) * gridHeight);
+        }
+
         public static bool InBetween(this int value, int minimum, int maximum)
         {
             return value >= minimum && value <= maximum;
         }
 
-        
+        public static bool IsInPosition(int x, int y, Tuple<int, int, int, int> pos)
+        {
+            return x.InBetween(pos.Item1, pos.Item3) && y.InBetween(pos.Item2, pos.Item4);
+        }
+
         public static Tuple<int, int> Rotate(int x, int y, int originX, int originY, Direction d)
         {
             double angleInRadians = 0;
