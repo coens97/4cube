dc8f0358785805009e4a4d0765ed0a9edde21aef
Coen "sleeping engineer" Stange <coen.stange@hotmail.com>
2016-05-30 09:32:16 +0200

    * Rewrite launcher layer

---
+++ Code/4cube.Launcher/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Ninject;
+using _4cube.Bussiness;
+using _4cube.Bussiness.Config;
+using _4cube.Bussiness.Simulation;
+using _4cube.Presentation.Window;
+using _4cube.Data;
+
+namespace _4cube.Launcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IKernel _container;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureContainer();
+            ComposeObjects();
+            Current.MainWindow.Show();
+        }
+
+        private void ConfigureContainer()
+        {
+            this._container = new StandardKernel();
+            _container.Bind<IGridModel>().To<GridModel>().InSingletonScope();
+            _container.Bind<IGridData>().To<GridData>().InTransientScope();
+            _container.Bind<ISimulation>().To<Simulation>().InTransientScope();
+            _container.Bind<MainWindow>().To<MainWindow>().InTransientScope();
+            _container.Bind<IConfig>().To<Config>().InSingletonScope();
+        }
+
+        private void ComposeObjects()
+        {
+            Current.MainWindow = this._container.Get<MainWindow>();
+        }
+    }
+}
